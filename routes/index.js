cad941ce5afcf1f6478d69e9274861dfb8aac6ab
Mayank <rajoria_m@yahoo.com>
2019-03-06 10:11:35 -0800

    push lab9 to json, changed dates

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,17 @@
 var labs = require('../labs.json');
 
 exports.view = function(req, res){
+  if(labs.length < 9) {
+  	var todaysLab = [{
+		"id": "lab-9",
+		"title": "Debugging 101",
+		"date": "March 7",
+		"image": "debug.png"
+	}];
+
+  	labs.push(todaysLab);
+  }
+
   res.render('index', {
 	'labs': labs
   });
